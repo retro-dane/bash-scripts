3fe43f4eda99be3c87bfdece8256a76587ae47b9
Khadane Hood <43159871+retro-dane@users.noreply.github.com>
2024-02-26 18:52:35 -0500

    Create update_resoolv.sh

---
+++ update_resoolv.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+## This script  will  update the current records in side your /etc/resolv.conf
+
+FILE=/etc/resolve.conf
+DECOMDNS=list_of_old_nameservers.txt
+NEWDNS=list_of_new_nameservers.txt
+COUNT= wc -l list_of_old_nameservers.txt
+
+## Searches for all old name servers and comments them out using a hashtag
+for i in $(cat DECOMDNS); do 
+    grep $i /etc/resolv.conf;
+    if  [ $? -eq 0 ]; then
+        sed 's/"$i"/#"$i"/' $FILE
+    fi
+done
+
+## appends all the new name servers to the  top of the list
+for i in $(cat NEWDNS); do 
+    grep $i /etc/resolv.conf;
+    LINE=cat -n /etc/resolv.conf | grep -m1 "nameserver" | awk '{print $1}'
+    if  [ $? -eq 0 ]; then
+        sed '$LINE $i ' $FILE
+    fi
+done
+
